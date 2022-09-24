91ba119a302d0c517ca60646d0bd2ba549c24876
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-24 20:41:44 +0900

    끝

---
+++ src/components/LoginForm.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from "@vue/test-utils";
+import LoginForm from "@/components/LoginForm.vue";
+
+describe("로그인 폼", () => {
+  test("ID는 이메일 형식이 아니면 경고 메시지가 출력된다.", () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: "test",
+        };
+      },
+    });
+    const warningText = wrapper.find(".warning");
+    console.log("경교메시지", warningText.html());
+    expect(warningText.exists()).toBeTruthy(); // 경교메시지가 존재한다.
+  });
+  test("ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다.", () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: "",
+          password: "",
+        };
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.element.disabled).toBeTruthy();
+  });
+});
