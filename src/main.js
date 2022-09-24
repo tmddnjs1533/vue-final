4c61b9e33bab368644d41545e918646652b0bb25
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-24 17:55:03 +0900

    -13 filter

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/routes/index";
 import store from "@/store/index";
+import { formatDate } from "@/utils/filters";
 
 Vue.config.productionTip = false;
+
+Vue.filter("formatDate", formatDate);
 
 new Vue({
   render: (h) => h(App),
