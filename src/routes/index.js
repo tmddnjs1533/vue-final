ff60137866eba8c2e3a142a8331a6bf2ff14702e
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-24 18:37:47 +0900

    14_router-navigation-guard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "@/store";
 // import LoginPage from '@/views/LoginPage.vue';
 // import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: "history",
   routes: [
     {
@@ -23,10 +24,16 @@ export default new VueRouter({
     {
       path: "/main",
       component: () => import("@/views/MainPage.vue"),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: "/add",
       component: () => import("@/views/PostAddPage.vue"),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: "*",
@@ -34,3 +41,23 @@ export default new VueRouter({
     },
   ],
 });
+
+/**
+ * VueRouter.beforeEach(guard: NavigationGuard): Function
+ * export type NavigationGuard<V>
+ * Alias for: (to: Route, from: Route, next: NavigationGuardNext<V>) => any
+ * to : 갈 곳
+ * from : 온 곳
+ * next : 가도 됨.
+ */
+router.beforeEach((to, from, next) => {
+  // 페이지 이동 전에 실행할 행동
+  if (to.meta.auth && !store.getters.isLogin) {
+    // 사용자 접근 제어
+    next("/login");
+    return;
+  }
+  next();
+});
+
+export default router;
